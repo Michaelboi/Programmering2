7194f18a51f79c1a650b4f7957378770590b5c5f
Michaelboi <micnwa050628@student.jenseneducation.se>
2022-10-18 13:41:39 +0200

    Add project files.

---
+++ Programmerings uppgifter/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lösenorduppgift
+{
+    public class information
+    {
+        public class lösenordinfo
+        {
+            private string lösenord = "";
+            public string visalösenord()
+            {
+                return this.lösenord;
+            }
+            public void setlösenord(string lösenord)
+            {
+                this.lösenord = lösenord;
+            }
+        }
+        
+        
+    }
+}
+namespace uppgift3
+{
+    public class Bank
+    {
+        public string Ägare;
+        public float Balance;
+        public string Kortnummer;
+
+        public Bank(string namn, float Totalpengar, string nummer)
+        {
+            this.Ägare = namn;
+            this.Balance = Totalpengar;
+            this.Kortnummer = nummer;
+        }
+    }
+}
---
+++ Programmerings uppgifter/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using lösenorduppgift;
+using uppgift3;
+
+/*namespace programmering
+{
+    class Lösenord
+    {
+        private string lösenord;
+        public string inputlösenord
+        {
+            return
+        }
+        
+    }
+}
+*/
+// Uppgift 1
+// Olle, du sa att min lösning var OK.
+/*void GenerateNumbers()
+{
+    // ger X ett random nummer
+    Random X = new Random();
+    int? value = X.Next(0, 10);
+    int[] numberX = new int[5];
+    // en funktion som tar bort duplicates
+    void RemoveDuplicate()
+    {
+        int[] removedup = numberX.Distinct().ToArray();
+        Array.ForEach(removedup, x => Console.WriteLine("X = " + x));
+    }
+    // sätter in 5 random värden från X i listan numberX
+    for (int i = 0; i < numberX.Length; i++)
+    {
+        numberX[i] = X.Next(0,10);
+    }
+    // kallar på funktionen som skannar listan efter duplicates och tar bort dom.
+    RemoveDuplicate();
+    // strunta i detta
+    if (numberX == null || numberX!.Contains(5))
+    {
+        RemoveDuplicate();
+    }
+
+}
+
+GenerateNumbers();
+
+    //instansierar random i X och Y variablerna
+    var X = new Random();
+    var Y = new Random();
+    //Den här skapar en lista i en variabel med 5 tomma celler med värden mellan 0-10
+    int[] numberX = new int[5]; 
+    Enumerable.Range(0, 6).OrderBy(x => X.Next()).Take(5).ToList();
+    int[] numberY = new int[5];
+    Enumerable.Range(0, 6).OrderBy(y => Y.Next()).Take(5).ToList();
+    //En loop som loopar igenom de olika variablerna som har har listor.
+    for (int i = 0; i < numberX.Length; i++)
+    {
+        Console.WriteLine("(" + numberX[i] + "," + numberY[i] + ")");
+        
+        
+    }
+        
+
+}
+GenerateNumbers();
+*/
+void Lösenord()
+{
+    // variabel för input
+    string input = "";
+    // skapar ny variabel från klassen
+    information.lösenordinfo sparatlösenord = new information.lösenordinfo();
+    Console.Write("Skapa lösenord mellan 3 till 10 karaktärer: ");
+    // loopar igenom så att man kan fortsätta efter att den har tagit in input som ett sparat lösenord.
+    while (input != "ex")
+    {
+        input = Console.ReadLine();
+        if (input.Length > 3 && input.Length < 10)
+        {
+            sparatlösenord.setlösenord(input);
+            Console.WriteLine("lösenord är sparat, vill du se ditt lösenord? Y/N");
+            if (input.Length < 3 || input.Length > 10)
+            {
+                Console.WriteLine("läs texten idiot");
+            }
+        }
+        
+        if (input == "Y")
+        {
+            Console.WriteLine(sparatlösenord.visalösenord());
+
+        }
+
+    }
+    
+}
+Lösenord();
+
+
+var konto1 = new Bank("Elias", 10000, "2021 2458 4935 1393");
+var konto2 = new Bank("Malkolm", 50000, "2195 9594 8348 1244");
