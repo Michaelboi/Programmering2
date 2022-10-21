836b5cceea52bb16ae554f12a6dfd151a91ceb6a
Michaelboi <95038340+Michaelboi@users.noreply.github.com>
2022-10-21 19:54:37 +0200

    Update Class1.cs

--- Programmerings uppgifter/Class1.cs
+++ Programmerings uppgifter/Class1.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,12 @@ namespace lösenorduppgift
         public class lösenordinfo
         {
             private string lösenord = "";
+            // printar lösenordet
             public string visalösenord()
             {
                 return this.lösenord;
             }
+            // sätter in ett valfritt lösenord som man valt
             public void setlösenord(string lösenord)
             {
                 this.lösenord = lösenord;
@@ -28,15 +31,17 @@ namespace uppgift3
 {
     public class Bank
     {
-        public string Ägare;
-        public float Balance;
-        public string Kortnummer;
-
+        // ger varje variabel en get och set funktion direkt utan en funktion
+        public string Ägare { get; set; }
+        public float Balance { get; }
+        public string Kortnummer { get; }
+        // en funktion som gör att man kan sätta in sina egna värden senare när man skapar nya variabler i en klass
         public Bank(string namn, float Totalpengar, string nummer)
         {
             this.Ägare = namn;
             this.Balance = Totalpengar;
-            this.Kortnummer = nummer;
+            this.Kortnummer = "kortnummer " + nummer;
+            
         }
     }
 }
